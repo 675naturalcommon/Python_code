6561fa087aeb14f6953d7d3ac4b7138d5fdaa0b1
675naturalcommon <168443467+675naturalcommon@users.noreply.github.com>
2025-03-29 16:38:13 +0800

    提交说明

--- plants_vs_zombies/from_book_part_7/input.py
+++ plants_vs_zombies/from_book_part_7/input.py
@@ -1,1 +1,9 @@
-print("Hello, world!")
+#input函数让程序暂停运行，等待用户输入一些文本。
+#input函数返回用户输入的字符串。
+#input函数的参数是提示信息，显示在用户输入时。
+
+message = input("Tell me something,and I will repeat it back to you: ")
+print(message)
+
+name = input("Please enter your name: ")
+print(f"Hello,{name.title()}!")
